606bb10ea9cf249aac67d0291533889178ba8737
Matt <matt@mattburns.co.uk>
2015-08-03 12:21:14 +0100

    Added basic authorization ability.

--- src/main/java/uk/co/mattburns/jest/JestForAppEngineClient.java
+++ src/main/java/uk/co/mattburns/jest/JestForAppEngineClient.java
@@ -7,7 +7,9 @@ import io.searchbox.client.AbstractJestClient;
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestResult;
 import io.searchbox.client.JestResultHandler;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.NotImplementedException;
+import org.apache.http.HttpStatus;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -19,14 +21,34 @@ import java.util.Set;
 
 public class JestForAppEngineClient implements JestClient {
 
-    private URL host;
+    private final URL host;
+    private final String username;
+    private final String password;
 
-    protected Gson gson = new GsonBuilder()
+    private final Gson gson = new GsonBuilder()
             .setDateFormat(AbstractJestClient.ELASTIC_SEARCH_DATE_FORMAT)
             .create();
 
+    /**
+     * JestClient that works on App Engine
+     *
+     * @param host Url of Elasticsearch node including port and protocol. eg "http://example.com:9200/" or "https://1.1.1.1:9999/"
+     */
     public JestForAppEngineClient(URL host) {
+        this(host, null, null);
+    }
+
+    /**
+     * JestClient that works on App Engine
+     *
+     * @param host Url of Elasticsearch node including port and protocol. eg "http://example.com:9200/" or "https://1.1.1.1:9999/"
+     * @param username for basic auth (pass null to ignore)
+     * @param password for basic auth (pass null to ignore)
+     */
+    public JestForAppEngineClient(URL host, String username, String password) {
         this.host = host;
+        this.username = username;
+        this.password = password;
     }
 
     @Override
@@ -40,6 +62,11 @@ public class JestForAppEngineClient implements JestClient {
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setDoOutput(true);
             connection.setRequestMethod(clientRequest.getRestMethodName());
+
+            if (username != null && password != null) {
+                // then set basic auth header
+                connection.setRequestProperty("Authorization", "Basic " + Base64.encodeBase64String((username + ":" + password).getBytes()));
+            }
 
             OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
             writer.write(payload);
@@ -57,6 +84,12 @@ public class JestForAppEngineClient implements JestClient {
             reasonPhrase += e.getMessage();
         }
 
+        if (responseCode != HttpURLConnection.HTTP_OK) {
+            if (reasonPhrase.isEmpty()) {
+                reasonPhrase = response;
+                response = "";
+            }
+        }
         return clientRequest.createNewElasticSearchResult(response, responseCode, reasonPhrase, gson);
     }
 
